b0c22021a10361ea4cb8fc0f468e9d9fbd49a170
Danil Shulgin <dvshulgin@yandex.ru>
2014-07-31 13:50:05 +0400

    add do

--- fabfile.py
+++ fabfile.py
@@ -8,6 +8,8 @@ env.password = 'Gbnth88'
 env.cwd = '/service/'
 preCommand = '/usr/bin/uptime'
 postCommand = '/bin/hostname'
+newFolder = 'new'
+bupFolder = 'bup'
 
 
 def mkdir_p(path):  # Создает директории
@@ -25,13 +27,25 @@ def listdir(path):  # Выдает список файлов/директори�
             pathToFiles.append(os.path.join(dirname, filename))
         if '.git' in dirnames:
             dirnames.remove('.git')
-    print(pathToFiles)
+    return(pathToFiles)
 
 
+def do():
+    #run(preCommand)
+    rFileList = listdir(newFolder)
+    for rFile in rFileList:
+        head, tail = os.path.split(env.cwd + rFile[len(newFolder) + 1:])
+        headBup, tailBup = os.path.split(bupFolder + "/" + rFile[len(newFolder) + 1:])
+        if exists(rFile[len(newFolder) + 1:]):
+            if not os.path.isdir(headBup):
+                mkdir_p_local(headBup)
+            get(rFile[len(newFolder) + 1:], headBup)
+        if not exists(head):
+            mkdir_p(head)
+        put(rFile, rFile[len(newFolder) + 1:])
+            #print(headBup)
 
-
-
-
+    #run(postCommand)
 
 
 def check_file(path):
